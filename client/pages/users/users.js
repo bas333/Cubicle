4c16127be05ff9254fb1d8444761cc5444e785ae
kyzzzhang <kyzzzhang@brandeis.edu>
2017-07-01 14:17:30 -0400

    user page2

--- client/pages/users/users.js
+++ client/pages/users/users.js
@@ -5,5 +5,44 @@ Template.users.helpers({
       a.push(i);
     }
     return a;
+  },
+  schoolData(){
+    return schools;
   }
 })
+const schools=[
+  {name:"Brandeis University"},
+  {name:"Boston University"},
+  {name:"Boston College"},
+  {name:"Babson College"},
+  {name:"Northeastern University"}
+]
+Template.users.events({
+  'click #submitnow':function(elt,instance) {
+    event.preventDefault();
+    const username=instance.$('#username').val();
+    const school=instance.$('#school').val();
+    const age=instance.$('#age').val();
+    var gender="";
+    if($('input[id="male"]').is(':checked')){
+      gender="male";
+    }else if($('input[id="female"]').is(':checked')){
+      gender="female";
+    }else{
+      gender="other";
+    };
+    console.log('adding '+name);
+    instance.$('#username').val("");
+    instance.$('#school').val("");
+    instance.$('#gender').val("");
+    instance.$('#age').val("");
+
+    var newUser={
+      username:username,
+      school:school,
+      gender:gender,
+      owner:Meteor.userId(),
+      createAt:new Date()
+    };
+    Meteor.call('users.insert',newUser);
+  }})
