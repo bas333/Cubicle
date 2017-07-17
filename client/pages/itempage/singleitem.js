8772a5d5dd8ad9601c292df22e35211a0862e682
apzlx <aprilzhou98@gmail.com>
2017-07-17 12:08:49 -0400

    add to cart on itempage

--- client/pages/itempage/singleitem.js
+++ client/pages/itempage/singleitem.js
@@ -3,3 +3,18 @@ if(Meteor.isClient){
       Meteor.subscribe('product');
     })
 }
+Template.singleitem.events({
+  'click #add'(elt,instance){
+    if (AllUsers.findOne({owner:Meteor.userId()})!=undefined){
+      Meteor.call('product.addcart',Meteor.userId(),this.p);
+    }else{
+      alert('Please log in first.')
+    }
+    alert('Item added to cart')
+  }
+})
+Template.singleitem.helpers({
+  isOwner(){
+    console.log(owner);
+    return (owner == Meteor.userId())}
+})
