0d637e59a6ba6ee7dd11b82a7b7755918a706f63
NinaXu317 <c1xu@brandeis.edu>
2017-07-17 14:18:01 -0400

    a

--- client/pages/itempage/singleitem.js
+++ client/pages/itempage/singleitem.js
@@ -3,3 +3,18 @@ if(Meteor.isClient){
       Meteor.subscribe('product');
     })
 }
+Template.singleitem.events({
+  'click #add'(elt,instance){
+    if (AllUsers.findOne({owner:Meteor.userId()})!=undefined){
+      Meteor.call('product.addcart',Meteor.userId(),this);
+    }else{
+      alert('Please log in first.')
+    }
+    alert('Item added to cart')
+  }
+})
+Template.singleitem.helpers({
+  isOwner(){
+    console.log(this.owner);
+    return (this.owner == Meteor.userId())}
+})
