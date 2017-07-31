a7e76242dc08458e9b9dec065ed4de7bf2ca260e
kyzzzhang <kyzzzhang@brandeis.edu>
2017-07-31 19:21:58 -0400

    reply

--- client/pages/forum/client/postforum.js
+++ client/pages/forum/client/postforum.js
@@ -3,3 +3,27 @@ Template.postforum.helpers({
     return Forum.find();
   },
 })
+Template.showpost.helpers({
+  isNotOwnPost(){
+    if(this.p.owner!=Meteor.userId()){
+      return true;
+    }
+  }
+})
+Template.showpost.events({
+  'click #submitreply':function(elt,instance){
+    var name = AllUsers.findOne({owner:Meteor.userId()}).username;
+    var now = new Date();
+    var text = instance.$("#replypost").val();
+    var replyId=this.p._id;
+    var reply = {
+      owner:Meteor.userId(),
+      name:name,
+      createdAt: now,
+      text: text,
+      replyId:replyId,
+    };
+    console.log(reply);
+    Meteor.call('reply.insert',reply);
+  }
+})
