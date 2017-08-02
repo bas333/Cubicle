e16c2bcf14bd60acc1634301213eeeee381c516a
NinaXu317 <c1xu@brandeis.edu>
2017-08-02 03:50:30 -0400

    login

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -4,5 +4,9 @@ Template.login.events ({
     var email = instance.$('#signin-email').val();
     var password = instance.$('#signin-password').val();
     Meteor.loginWithPassword(email,password);
+    var login = Meteor.users.findOne(Meteor.userId());
+    if (login) {
+      Router.go("users");
+    }
   }
 })
